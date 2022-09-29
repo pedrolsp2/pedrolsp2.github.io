c3a40bee5e944e5f0c5f6d290cbe009337a376e4
Your Name <you@example.com>
2022-09-29 17:20:12 -0300

    up

--- script.js
+++ script.js
@@ -1,46 +1,120 @@
-// Lógica
-// 1 - Selecionar elementos que devem ser animados
-// 2 - Definir a classe que é adicionada durante a animação
-// 3 - Criar função de animação
-// 3.1 - Verificar a distância entre a barra de scroll e o topo do site
-// 3.2 - Verificar se a distância do 3.1 + Offset é maior do que a distância entre o elemento e o Topo da Página.
-// 3.3 - Se verdadeiro adicionar classe de animação, remover se for falso.
-// 4 - Ativar a função de animação toda vez que o usuário utilizar o Scroll
-// 5 - Otimizar ativação
-// Debounce do Lodash
-const debounce = function(func, wait, immediate) {
-  let timeout;
-  return function(...args) {
-    const context = this;
-    const later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
+const btnMobile = document.getElementById('btn-celular');
 
-const target = document.querySelectorAll('[data-anime]');
-const animationClass = 'animate';
+function toggleMenu(event){
+    if (event.type == 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
 
-function animeScroll() {
-  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 6);
-  target.forEach(function(element) {
-    if((windowTop) > element.offsetTop) {
-      element.classList.add(animationClass);
-    } else {
-      element.classList.remove(animationClass);
-    }
-  })
+    if(active){
+       event.currentTarget.setAttribute('aria-label','Fechar Menu');
+    } else{
+        
+    } event.currentTarget.setAttribute('aria-label','Abrir Menu');
+    
 }
 
-animeScroll();
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
 
-if(target.length) {
-  window.addEventListener('scroll', debounce(function() {
-    animeScroll();
-  }, 200));
+
+
+window.addEventListener('scroll', function(){
+    var menu = document.querySelector('header');
+    menu.classList.toggle('sticky', window.scrollY > 200);
+  })
+
+  window.addEventListener('scroll', function(){
+    var menu = document.querySelector('.logo');
+    menu.classList.toggle('sticky2', window.scrollY > 200);
+  })
+
+  window.addEventListener('scroll', function(){
+    var menu = document.querySelector(' #btn-celular');
+    menu.classList.toggle('sticky3', window.scrollY > 200);
+  })
+
+  window.addEventListener('scroll', function(){
+    var menu = document.querySelector('#icon-menu');
+    menu.classList.toggle('sticky4', window.scrollY > 200);
+  })
+ 
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+String.prototype.reverse = function(){
+  return this.split('').reverse().join(''); 
+};
+
+function mascaraMoeda(campo,evento){
+  var tecla = (!evento) ? window.event.keyCode : evento.which;
+  var valor  =  campo.value.replace(/[^\d]+/gi,'').reverse();
+  var resultado  = "";
+  var mascara = "R$##.###.###,##".reverse();
+  for (var x=0, y=0; x<mascara.length && y<valor.length;) {
+    if (mascara.charAt(x) != '#') {
+      resultado += mascara.charAt(x);
+      x++;
+    } else {
+      resultado += valor.charAt(y);
+      y++;
+      x++;
+    }
+  }
+  campo.value = "R$ " + resultado.reverse();
 }
