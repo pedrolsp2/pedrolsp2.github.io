32c1435b45425ac31f79c15bf134d203d0079a53
pedrolsp2 <54607320+pedrolsp2@users.noreply.github.com>
2023-01-05 09:28:14 -0300

    Add files via upload

---
+++ Login Original/hotspot/script.js
@@ -0,0 +1,42 @@
+function Sumir() {
+    var status = false;
+    let checkbox = document.getElementById('aceito1');
+    let checkbox2 = document.getElementById('aceito2');
+    if (checkbox.checked && checkbox2.checked) {
+        status = true;
+    } else {
+        status = false;
+    }
+
+    if (status == true) {
+        var adicionar = document.getElementById("botoes");
+        adicionar.classList.add("heartbeat");
+        document.getElementById('menssagem-alerta').classList.remove("hide");
+    }
+    else {
+        var adicionar = document.getElementById("botoes");
+        adicionar.classList.add("heartbeat");
+        document.getElementById('menssagem-alerta').classList.remove("hide");
+    }
+}
+
+function Esconder() {
+    var status = false;
+    let checkbox = document.getElementById('aceito1');
+    let checkbox2 = document.getElementById('aceito2');
+    if (checkbox.checked && checkbox2.checked) {
+        status = true;
+    } else {
+        status = false;
+    }
+
+    if (status == true) {
+        var adicionar = document.getElementById("politica");
+        adicionar.classList.add("scale-out-center");
+    }
+    else {
+        var adicionar = document.getElementById("botoes");
+        adicionar.classList.add("heartbeat");
+        document.getElementById('menssagem-alerta').classList.remove("hide");
+    }
+}
